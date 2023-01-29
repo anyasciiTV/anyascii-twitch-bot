e73c28e219d0f3c93438a56b389a8c5a040c6bc6
anyascii <119780229+anyasciiTV@users.noreply.github.com>
2023-01-29 12:16:43 -0800

    Initial commit
    The .env file that stores the Twitch IRC bot's environment variables.

---
+++ .env.py
@@ -0,0 +1,7 @@
+# Environment variables used by TwitchIO IRC bot
+
+IRC_TOKEN = 'TMI_TOKEN' # TMI token
+CLIENT_ID = 'CLIENT_ID' # Client ID 
+BOT_NAME = 'BOT_NAME' # Include the Twitch bot's name
+PREFIX = '!' # Prefix prior to the channel command 
+CHANNEL_NAME = 'CHANNEL_NAME' # The channel command 
